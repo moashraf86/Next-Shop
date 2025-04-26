933209ca91e207d858e59703cd36eedb8a4d2f4c
moashraf86 <botoashraf123@gmail.com>
2025-04-26 18:03:56 +0300

    feat(related-products): implement carousel for displaying related products and enhance layout for better responsiveness

--- components/product/RelatedProducts.tsx
+++ components/product/RelatedProducts.tsx
@@ -1,6 +1,13 @@
 import ProductCard from "@/app/products/ProductCard";
 import { fetchProductsByCategory } from "@/lib/data";
 import { Product } from "@/lib/definitions";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 
 export default async function RelatedProducts({
   product,
@@ -36,11 +43,36 @@ export default async function RelatedProducts({
         >
           Available styles
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-4">
-          {filteredProducts.map((relatedProduct) => (
-            <ProductCard key={relatedProduct.id} product={relatedProduct} />
+        {/* Visible on screens < 1024px */}
+        <div className="grid auto-cols-[52vw] md:auto-cols-[35vw] grid-cols-none grid-flow-col gap-5 overflow-x-auto snap-x snap-mandatory scrollbar-hide lg:hidden">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <div key={index} className="snap-center snap-always">
+              <ProductCard
+                product={filteredProducts[index % filteredProducts.length]}
+              />
+            </div>
           ))}
         </div>
+        {/* Visible on screens > 1024px */}
+        <Carousel
+          className="w-full hidden lg:flex"
+          opts={{ align: "start", slidesToScroll: 4 }}
+        >
+          <CarouselContent className="-ml-6">
+            {Array.from({ length: 8 }).map((_, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-6 md:basis-1/2 lg:basis-1/4"
+              >
+                <ProductCard
+                  product={filteredProducts[index % filteredProducts.length]}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="w-14 h-14 rounded-none -left-7 top-[calc(50%-25px)] shadow-none disabled:hidden" />
+          <CarouselNext className="w-14 h-14 rounded-none -right-7 top-[calc(50%-25px)] shadow-none disabled:hidden" />
+        </Carousel>
       </div>
     </div>
   );
