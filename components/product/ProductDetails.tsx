3e18568676578ae8e403f303a11cc116e1570456
moashraf86 <botoashraf123@gmail.com>
2025-04-28 20:24:20 +0300

    feat(product-details, related-products): refactor layout to use semantic HTML elements and improve structure for better readability

--- components/product/ProductDetails.tsx
+++ components/product/ProductDetails.tsx
@@ -29,82 +29,84 @@ export default function ProductDetails({
   );
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 lg:gap-10 mb-20">
-      {/* Product Carousel */}
-      <ProductCarousel images={product.images} className="lg:col-span-7" />
-      {/* Product details */}
-      <div className="space-y-6 lg:col-span-5">
-        <div className="space-y-6 border-b border-border pb-4">
-          <ProductTitle title={product.name} />
-          <div className="flex items-center gap-1 text-lg lg:text-2xl  font-normal font-barlow">
-            <ProductPrice
-              price={product.price}
-              className="text-lg lg:text-2xl font-barlow font-normal"
-            />
-            USD
+    <section className="container max-w-screen-xl">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-6 lg:gap-10 mb-20">
+        {/* Product Carousel */}
+        <ProductCarousel images={product.images} className="lg:col-span-7" />
+        {/* Product details */}
+        <div className="space-y-6 lg:col-span-5">
+          <div className="space-y-6 border-b border-border pb-4">
+            <ProductTitle title={product.name} />
+            <div className="flex items-center gap-1 text-lg lg:text-2xl  font-normal font-barlow">
+              <ProductPrice
+                price={product.price}
+                className="text-lg lg:text-2xl font-barlow font-normal"
+              />
+              USD
+            </div>
+            <ProductRating rating={5} reviews={3} />
           </div>
-          <ProductRating rating={5} reviews={3} />
-        </div>
-        <VariantSelector
-          variant={product.variantOptions[0]}
-          selectedVariant={selectedVariant}
-          setSelectedVariant={setSelectedVariant}
-        />
-        <div className="space-y-1">
-          <span>Quantity:</span>
-          <QuantitySelector
-            quantity={quantity}
-            setQuantity={setQuantity}
-            mode="product"
+          <VariantSelector
+            variant={product.variantOptions[0]}
+            selectedVariant={selectedVariant}
+            setSelectedVariant={setSelectedVariant}
           />
+          <div className="space-y-1">
+            <span>Quantity:</span>
+            <QuantitySelector
+              quantity={quantity}
+              setQuantity={setQuantity}
+              mode="product"
+            />
+          </div>
+          <ProductActions
+            product={product}
+            quantity={quantity}
+            selectedVariant={selectedVariant}
+          />
+          <Accordion type="single" collapsible>
+            <AccordionItem value="description">
+              <AccordionTrigger className="text-sm font-barlow uppercase font-semibold tracking-[1px] hover:no-underline">
+                Description
+              </AccordionTrigger>
+              <AccordionContent>
+                <ProductDescription description={product.description} />
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="shipping">
+              <AccordionTrigger className="text-sm font-barlow uppercase font-semibold tracking-[1px] hover:no-underline">
+                Shipping & Returns
+              </AccordionTrigger>
+              <AccordionContent className="font-barlow space-y-2">
+                <div className="space-y-1">
+                  <strong>Worldwide free shipping</strong>
+                  <p>
+                    We use DHL Express for worldwide shipping. Delivery time is
+                    usually 2-4 working days. NB: For Countries outside EU
+                    buying items ex. VAT, be aware you have to pay import taxes
+                    according to the laws of that specific country. In case of
+                    returns we are not able to return any duties or taxes, as
+                    this is paid to and handled directly between you (the
+                    customer) and your country.
+                  </p>
+                </div>
+                <div className="space-y-1">
+                  <strong>Return policy</strong>
+                  <p>
+                    If you want to change a product into another size, color
+                    etc, please contact us so we are able to reserve the new
+                    item in our stock immediately. You are always entitled to an
+                    exchange or refund within 30 days after you have received
+                    your package, as long as the item has not been used. All
+                    original packaging, price labels etc. shall be returned with
+                    the product without having been tampered with.
+                  </p>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
-        <ProductActions
-          product={product}
-          quantity={quantity}
-          selectedVariant={selectedVariant}
-        />
-        <Accordion type="single" collapsible>
-          <AccordionItem value="description">
-            <AccordionTrigger className="text-sm font-barlow uppercase font-semibold tracking-[1px] hover:no-underline">
-              Description
-            </AccordionTrigger>
-            <AccordionContent>
-              <ProductDescription description={product.description} />
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="shipping">
-            <AccordionTrigger className="text-sm font-barlow uppercase font-semibold tracking-[1px] hover:no-underline">
-              Shipping & Returns
-            </AccordionTrigger>
-            <AccordionContent className="font-barlow space-y-2">
-              <div className="space-y-1">
-                <strong>Worldwide free shipping</strong>
-                <p>
-                  We use DHL Express for worldwide shipping. Delivery time is
-                  usually 2-4 working days. NB: For Countries outside EU buying
-                  items ex. VAT, be aware you have to pay import taxes according
-                  to the laws of that specific country. In case of returns we
-                  are not able to return any duties or taxes, as this is paid to
-                  and handled directly between you (the customer) and your
-                  country.
-                </p>
-              </div>
-              <div className="space-y-1">
-                <strong>Return policy</strong>
-                <p>
-                  If you want to change a product into another size, color etc,
-                  please contact us so we are able to reserve the new item in
-                  our stock immediately. You are always entitled to an exchange
-                  or refund within 30 days after you have received your package,
-                  as long as the item has not been used. All original packaging,
-                  price labels etc. shall be returned with the product without
-                  having been tampered with.
-                </p>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
       </div>
-    </div>
+    </section>
   );
 }
--- components/product/RelatedProducts.tsx
+++ components/product/RelatedProducts.tsx
@@ -38,45 +38,43 @@ export default async function RelatedProducts({
   if (filteredProducts.length === 0) return null;
 
   return (
-    <div className="col-span-full mb-20">
-      <div className="space-y-10">
-        <h2
-          className="text-3xl md:text-4xl lg:text-5xl text-center font-light uppercase leading-tight tracking-tight"
-          aria-label={`More products like ${product.name}`}
-        >
-          Available styles
-        </h2>
-        {/* Visible on screens < 1024px */}
-        <div className="grid auto-cols-[52vw] md:auto-cols-[35vw] grid-cols-none grid-flow-col gap-5 overflow-x-auto snap-x snap-mandatory scrollbar-hide lg:hidden">
+    <section className="container max-w-screen-xl mb-20 space-y-10">
+      <h2
+        className="text-3xl md:text-4xl lg:text-5xl text-center font-light uppercase leading-tight tracking-tight"
+        aria-label={`More products like ${product.name}`}
+      >
+        Available styles
+      </h2>
+      {/* Visible on screens < 1024px */}
+      <div className="grid auto-cols-[52vw] md:auto-cols-[35vw] grid-cols-none grid-flow-col gap-5 overflow-x-auto snap-x snap-mandatory scrollbar-hide lg:hidden">
+        {Array.from({ length: filteredProducts.length }).map((_, index) => (
+          <div key={index} className="snap-center snap-always">
+            <ProductCard
+              product={filteredProducts[index % filteredProducts.length]}
+            />
+          </div>
+        ))}
+      </div>
+      {/* Visible on screens > 1024px */}
+      <Carousel
+        className="w-full hidden lg:flex"
+        opts={{ align: "start", slidesToScroll: 4 }}
+      >
+        <CarouselContent className="-ml-6">
           {Array.from({ length: filteredProducts.length }).map((_, index) => (
-            <div key={index} className="snap-center snap-always">
+            <CarouselItem
+              key={index}
+              className="pl-6 md:basis-1/2 lg:basis-1/4"
+            >
               <ProductCard
                 product={filteredProducts[index % filteredProducts.length]}
               />
-            </div>
+            </CarouselItem>
           ))}
-        </div>
-        {/* Visible on screens > 1024px */}
-        <Carousel
-          className="w-full hidden lg:flex"
-          opts={{ align: "start", slidesToScroll: 4 }}
-        >
-          <CarouselContent className="-ml-6">
-            {Array.from({ length: filteredProducts.length }).map((_, index) => (
-              <CarouselItem
-                key={index}
-                className="pl-6 md:basis-1/2 lg:basis-1/4"
-              >
-                <ProductCard
-                  product={filteredProducts[index % filteredProducts.length]}
-                />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious className="w-14 h-14 rounded-none -left-7 top-[calc(50%-25px)] shadow-none disabled:hidden" />
-          <CarouselNext className="w-14 h-14 rounded-none -right-7 top-[calc(50%-25px)] shadow-none disabled:hidden" />
-        </Carousel>
-      </div>
-    </div>
+        </CarouselContent>
+        <CarouselPrevious className="w-14 h-14 rounded-none -left-7 top-[calc(50%-25px)] shadow-none disabled:hidden" />
+        <CarouselNext className="w-14 h-14 rounded-none -right-7 top-[calc(50%-25px)] shadow-none disabled:hidden" />
+      </Carousel>
+    </section>
   );
 }
