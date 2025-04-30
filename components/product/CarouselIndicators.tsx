fdf547601e0eaa9f2b9880d18f2adb76443ea6df
moashraf86 <botoashraf123@gmail.com>
2025-04-30 22:32:39 +0300

    feat: update CarouselIndicators and carousel components to improve styling and replace arrow icons with chevrons

--- components/product/CarouselIndicators.tsx
+++ components/product/CarouselIndicators.tsx
@@ -7,25 +7,30 @@ export default function CarouselIndicators({
   current,
   handleScrollToImage,
   indicatorsMaxHeight,
+  className,
 }: {
   images: StrapiImage[];
   current: number;
   handleScrollToImage: (index: number) => void;
   indicatorsMaxHeight?: number;
+  className?: string;
 }) {
   return (
     <div
-      className="lg:overflow-y-auto lg:min-w-fit scrollbar-hide"
+      className={cn(
+        "lg:overflow-y-auto lg:min-w-fit scrollbar-hide",
+        className
+      )}
       style={{
         maxHeight: indicatorsMaxHeight ? `${indicatorsMaxHeight}px` : "none",
       }}
     >
-      <div className="grid grid-flow-col auto-cols-[24vw] sm:auto-cols-[20vw] md:auto-cols-auto gap-2 p-2 lg:flex lg:flex-col snap-x snap-mandatory overflow-x-auto scrollbar-hide lg:overflow-x-visible">
+      <div className="grid grid-flow-col sm:auto-cols-[15vw] md:auto-cols-[12vw] gap-2 p-2 lg:flex lg:flex-col snap-x snap-mandatory overflow-x-auto scrollbar-hide lg:overflow-x-visible">
         {images.map((image, index) => (
           <button
             key={image.id}
             className={cn(
-              "relative w-full aspect-square sm:aspect-auto sm:h-32 lg:w-16 lg:h-20 xl:w-20 xl:h-24 p-3 after:absolute after:content-[''] after:inset-[-3px] after:bg-transparent after:border-2 after:border-primary after:z-[-1] after:transition-transform after:duration-200 after:ease",
+              "relative w-full aspect-[9/11] lg:w-16 lg:h-20 xl:w-20 xl:h-24 p-3 after:absolute after:content-[''] after:inset-[-3px] after:bg-transparent after:border-2 after:border-primary after:z-[-1] after:transition-transform after:duration-200 after:ease",
               {
                 " after:scale-100 after:opacity-100": current === index,
                 "after:scale-90 after:opacity-0": current !== index,
@@ -34,8 +39,8 @@ export default function CarouselIndicators({
             onClick={() => handleScrollToImage(index)}
           >
             <Image
-              src={images[index].formats?.small?.url || images[index].url}
-              alt={images[index].alternativeText || "Product Image"}
+              src={images[index].formats?.small?.url}
+              alt={images[index].alternativeText || "Indicator image"}
               fill
               quality={100}
               className="object-cover object-center"
