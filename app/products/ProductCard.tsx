a8a2905ed8444fff99c2537b1fdb90ce64464001
moashraf86 <botoashraf123@gmail.com>
2025-05-04 20:07:07 +0300

    refactor: comment out bestselling logic in ProductCard component

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function ProductCard({ product }: { product: Product }) {
-  const isBestselling = product.categories.some(
-    (cat) => cat.name === "bestselling"
-  );
+  // const isBestselling = product.categories.some(
+  //   (cat) => cat.name === "bestselling"
+  // );
 
   const size = product.sizes?.[0]?.value ?? null;
   const color = product.sizes?.[0]?.colors?.[0]?.name ?? null;
@@ -30,11 +30,11 @@ export default function ProductCard({ product }: { product: Product }) {
           fill
           sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
         />
-        {isBestselling && (
+        {/* {isBestselling && (
           <span className="absolute top-2 inline-block left-2 py-px px-[5px] bg-orange-500 text-primary-foreground text-[10px] uppercase font-barlow font-semibold">
             Bestselling
           </span>
-        )}
+        )} */}
       </div>
       <div className="px-2 space-y-2">
         <h2 className="text-sm text-center font-barlow">{product.name}</h2>
