51c96e5b80e714be6e55cf28a7925aa349d7a6c6
moashraf86 <botoashraf123@gmail.com>
2025-05-05 19:53:19 +0300

    feat: add TopProgressBar component for loading indication; implement loading state in ProductSorting component; update package.json and package-lock.json to include @radix-ui/react-progress dependency

--- components/product/ProductSorting.tsx
+++ components/product/ProductSorting.tsx
@@ -1,14 +1,18 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Select, SelectContent, SelectItem, SelectTrigger } from "../ui/select";
+import { useState, useTransition } from "react";
+import TopProgressBar from "../shared/TopProgressBar";
 
 export default function ProductSorting() {
   // get search params from URL
   const searchParams = useSearchParams();
   const sortByParam = searchParams.get("sort_by");
   const URL = useRouter();
-
   const currentSort = searchParams.get("sort_by") || "createdAt:desc";
+
+  const [isPending, startTransition] = useTransition();
+  const [isLoading, setIsLoading] = useState(false);
 
   // Define sorting options
   const sortingOptions = [
@@ -22,27 +26,37 @@ export default function ProductSorting() {
 
   // Handle sorting change
   const handleSortingChange = (value: string) => {
-    URL.push(`?sort_by=${value}`, { scroll: false });
+    setIsLoading(true);
+    // simulate a delay to show loading state
+    setTimeout(() => {
+      startTransition(() => {
+        URL.push(`?sort_by=${value}`, { scroll: false });
+      });
+      setIsLoading(false);
+    }, 350);
   };
 
   return (
-    <div className="flex items-center gap-1">
-      <span className="text-sm whitespace-nowrap text-gray-500">Sort by</span>
-      <Select defaultValue={currentSort} onValueChange={handleSortingChange}>
-        <SelectTrigger className="border-none gap-1">
-          <span className="text-sm font-barlow">
-            {sortingOptions.find((option) => option.value === sortByParam)
-              ?.label || "Latest"}
-          </span>
-        </SelectTrigger>
-        <SelectContent>
-          {sortingOptions.map((option) => (
-            <SelectItem key={option.value} value={option.value}>
-              <span className="text-sm font-barlow">{option.label}</span>
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-    </div>
+    <>
+      <TopProgressBar trigger={isLoading || isPending} />
+      <div className="flex items-center gap-1">
+        <span className="text-sm whitespace-nowrap text-gray-500">Sort by</span>
+        <Select defaultValue={currentSort} onValueChange={handleSortingChange}>
+          <SelectTrigger className="border-none gap-1">
+            <span className="text-sm font-barlow">
+              {sortingOptions.find((option) => option.value === sortByParam)
+                ?.label || "Latest"}
+            </span>
+          </SelectTrigger>
+          <SelectContent>
+            {sortingOptions.map((option) => (
+              <SelectItem key={option.value} value={option.value}>
+                <span className="text-sm font-barlow">{option.label}</span>
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+    </>
   );
 }
